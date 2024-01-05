94b148620bde42a1faed86809aa619e1725c782c
sandrajade <bergersandra63@gmail.com>
2024-01-05 17:14:30 +0100

    data recovery

--- resources/views/tipsy/cocktails.blade.php
+++ resources/views/tipsy/cocktails.blade.php
@@ -6,19 +6,9 @@
                 <div class="flex flex-wrap justify-between gap-4">
                     <a href="{{ route('cocktails') }}"
                         class="relative flex flex-col hover:rotate-1 transition-all text-center font-pacifico text-xl  hover:saturate-200 p-2 rounded-lg shadow-xl cursor-pointer {{ $this->filterTheme == null ? ' rotate-1 ring-offset-4 ring-2 ring-bleu_clair ring-offset-transparent' : 'bg-bleu_clair' }}">Tous</a>
-                    <a href="{{ route('cocktails') }}"
-                        class="relative flex flex-col hover:rotate-1 transition-all text-center font-pacifico text-xl  hover:saturate-200 p-2 rounded-lg shadow-xl cursor-pointer {{ $this->filterTheme == null ? ' rotate-1 ring-offset-4 ring-2 ring-bleu_clair ring-offset-transparent' : 'bg-bleu_clair' }}">Tous</a>
+                   
                     @foreach ($currentThemes as $theme)
-                        @php
-                            $minus = Arr::random(['-', '']);
-                            $angle = Arr::random(['2', '3', '6']);
-                        @endphp
-                        <input type="radio" id="{{ $theme->id }}" value="{{ $theme->id }}"
-                            {{ $this->filterTheme == $theme->id ? 'checked' : '' }}
-                            class="absolute h-0 w-0 appearance-none ">
-                        <label for="{{ $theme->id }}"
-                            class="relative flex flex-col hover:rotate-1 transition-all text-center py-2 px-4 font-pacifico text-xl  hover:saturate-200  rounded-lg shadow-xl cursor-pointer {{ $this->filterTheme == $theme->id ? ' rotate-1 ring-offset-4 ring-2 ring-bleu_clair ring-offset-transparent' : $minus . 'rotate-' . $angle . ' bg-' . $theme->color . '-300' }}">{{ $theme->name }}
-                        </label>
+                      
                         @php
                             $minus = Arr::random(['-', '']);
                             $angle = Arr::random(['2', '3', '6']);
